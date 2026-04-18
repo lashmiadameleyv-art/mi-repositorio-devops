98634a33809f5c8e70b854d8a8e57a807056c0f5
Ubuntu <ubuntu@ip-172-31-32-184.ec2.internal>
2026-04-18 01:26:17 +0000

    Actualización automática Jenkins

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hola, aplicación Flask funcionando en Docker y Jenkins"
+    return "Nueva versión desplegada automáticamente con Jenkins CI/CD"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
