cc719947814ebd29acece90667e27ef2b55319a0
cygnux33 <osobro2006@gmail.com>
2026-04-21 08:32:42 -0600

    Update home route response message

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Nueva versión desplegada automáticamente con Jenkins CI/CD, De nuevo cambios echos por Cesar Avalos desde VSC"
+    return "Nueva versión desplegada automáticamente con Jenkins CI/CD, De nuevo cambios echos por Cesar Avalos desde VSC,Hola muy buenas a todos guapisimos aqui Alonso Montiel Ortega y hoy estamos en Minecraft"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
