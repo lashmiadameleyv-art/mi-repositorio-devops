6f16476c261b057d9f9cd90d5d1fd6310c65dcdc
0bby1 <cesarava06@gmail.com>
2026-04-21 08:11:58 -0600

    Commit

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Nueva versión desplegada automáticamente con Jenkins CI/CD, Aaaaaaaaaaa cambios echos por Cesar Avalos"
+    return "Nueva versión desplegada automáticamente con Jenkins CI/CD, De nuevo cambios echos por Cesar Avalos desde VSC"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
